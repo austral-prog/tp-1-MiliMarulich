369bd55490d605a52fa4941ccced69ce0477fb5e
MiliMarulich <mmarulich@mail.austral.edu.ar>
2025-03-21 11:48:42 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    sum = a + b
+    print(sum)
+    difference = a - b 
+    print(difference)
+    product = a * b 
+    print(product)
+    average = (a + b) / 2
+    print(average)
+    integer_division = a // b 
+    print(integer_division)
+    module_division = a % b 
+    print(module_division)
+    division = a / b 
+    print(division)
+math()
